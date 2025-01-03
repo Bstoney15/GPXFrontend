8c2cdbc331df6e2d908a32ee55c847669ee40fde
willbtty <willem.battey@gmail.com>
2025-01-02 23:37:37 -0600

    p2 of last commit

--- GPXApp/ContentView.swift
+++ GPXApp/ContentView.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 import MapKit
 import UIKit
+import Combine
 
 struct ContentView: View {
     @State private var osmData: OSMResponse = OSMResponse(version: 0.0, generator: "", osm3s: [:], elements: [])
+    @State private var position: MapCameraPosition = .automatic
+    
+    // Location Manager stuff
+    @StateObject var deviceLocationService = DeviceLocationService.shared
+    
+    @State var tokens: Set<AnyCancellable> = []
+    @State var coordinates: (lat: Double, lon: Double) = (0.0, 0.0)
+    
     var body: some View {
         VStack {
+            Text("Longitude: \(coordinates.lon)")
+                .font(.headline)
+            Text("Latitude: \(coordinates.lat)")
+                .font(.headline)
+            
             Map {
-
+                
             }
             .mapStyle(.hybrid(elevation: .realistic))
             .safeAreaInset(edge: .bottom) {
                 HStack {
                     Button(action: {
-                        print("Button pressed")
+                        
                     }) {
                         Text("Tap me")
                             .foregroundColor(.white)
@@ -39,6 +53,32 @@ struct ContentView: View {
                 print("OSM Shit failed", error)
             }
         }
+        .onAppear() {
+            observeCoordinateUpdates()
+            observeDeviceLocationDenied()
+            deviceLocationService.requestLocationUpdates()
+        }
+    }
+    
+    
+    func observeCoordinateUpdates() {
+        deviceLocationService.coordinatesPublisher
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                print("Handle \(completion) for error and finished subscription.")
+            } receiveValue: { coordinates in
+                self.coordinates = (coordinates.latitude, coordinates.longitude)
+            }
+            .store(in: &tokens)
+    }
+    
+    func observeDeviceLocationDenied() {
+        deviceLocationService.deniedLocationAccess
+            .receive(on: DispatchQueue.main)
+            .sink {
+                print("Error on getting location")
+            }
+            .store(in: &tokens)
     }
 }
 
